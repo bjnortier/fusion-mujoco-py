3a6d83b09901a8c206cd771ac5e1c056b2ab2b81
Ben Nortier <ben@bjnortier.com>
2018-04-06 15:56:41 +0200

    Basic module from sample structure

---
+++ README.rst
@@ -0,0 +1,8 @@
+# fusion-mujoco-py
+
+## Getting Started
+
+    $ python -m venv env
+    $ source env/bin/activate
+    $ make init
+    $ make test
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='fusion-mujoco-py',
+    version='0.1.0',
+    description='Export Autodesk Fusion to Mujoco XML model',
+    long_description=readme,
+    author='Ben Nortier',
+    author_email='ben@bjnortier.com',
+    url='https://github.com/bjnortier/fusion-mujoco-py',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
